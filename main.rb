1431121af52e5d690debb3935ae5396a8007fe14
Folarin Soboyejo <ss79418@ex.kroger.com>
2015-01-13 14:02:54 -0500

    Added App Models:User, Groups, Hackathons and Project

--- main.rb
+++ main.rb
@@ -2,7 +2,87 @@ require 'sinatra'
 require 'json'
 require 'data_mapper'
 
+DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :email, String
+  property :created_by, String
+  property :last_update_by, String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  has n, :usergroups
+  has n, :groups, :through => :usergroups
+end
+
+class Project
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :group_id, Serial
+  property :name, String
+  property :description, Text
+  property :status, String
+  property :avatar, String
+  property :repo, String
+  property :created_by, String
+  property :last_update_by, String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  belongs_to :group
+
+end
+
+class Hackathon
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :group_id, Serial
+  property :name, String
+  property :hack_date, DateTime
+  property :created_by, String
+  property :last_update_by, String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  belongs_to :group
+
+end
+
+class Group
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :created_by, String
+  property :last_update_by, String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  has n, :hackathons
+  has n, :projects
+
+  has n, :usergroups
+  has n, :users, :through => :usergroups
+end
+
+class Usergroup
+  include DataMapper::Resource
+  property :id, Serial
+  property :created_at, DateTime
+
+  belongs_to :user
+  belongs_to :group
+
+end
+
+DataMapper.finalize
 
 get '/heartbeat' do
   response = {status: 'alive'}
--- test.rb
+++ test.rb
@@ -20,3 +20,53 @@ describe "Get /heartbeat" do
 
   it { response['status'].must_equal 'alive'}
 end
+
+describe User do
+  let(:user) { User.new }
+
+  it "is an instance of User" do
+    assert_instance_of User, user
+  end
+
+  it "is a valid User" do
+    user.valid?.must_equal true
+  end
+
+end
+
+describe Project do
+  let(:project) { Project.new }
+
+  it "is an instance of Project" do
+    assert_instance_of Project, project
+  end
+
+  it "is a valid Project" do
+    project.valid?.must_equal true
+  end
+end
+
+describe Hackathon do
+  let(:hackathon) { Hackathon.new(name: 'Hackathon 3', created_by: 'Eric G') }
+
+  it "is an instance of Hackathon" do
+    assert_instance_of Hackathon, hackathon
+  end
+
+  it "is a valid Hackathon" do
+    puts hackathon
+    hackathon.valid?.must_equal true
+  end
+end
+
+describe Group do
+  let(:group) { Group.new }
+
+  it "is an instance of Group" do
+    assert_instance_of Group, group
+  end
+
+  it "is a valid Group" do
+    group.valid?.must_equal true
+  end
+end
